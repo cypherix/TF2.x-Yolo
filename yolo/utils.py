c916f11f30e9dc6d8c7da2b3483271250be1e7e8
Arjun Muraleedharan <arjun.m@iinerds.com>
2020-08-16 21:01:55 +0530

    Added dataset class with iterators

--- yolo/utils.py
+++ yolo/utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import cv2
 import numpy as np
 import tensorflow as tf
 
@@ -61,3 +61,24 @@ def giou(bboxes1, bboxes2):
     g_area = tf.maximum(
         (g_box[..., 2] - g_box[..., 0]) * (g_box[..., 3] - g_box[..., 1]), 0.0)
     return iou - (g_area - union_area) / g_area
+
+
+def image_preprocess(image, target_size, gt_boxes=None):
+    ih = iw = target_size
+    h, w, _ = image.shape
+
+    scale = min(iw/w, ih/h)
+    nw, nh = int(scale * w), int(scale * h)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
+    dw, dh = (iw - nw) // 2, (ih-nh) // 2
+    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    image_paded = image_paded / 255.
+
+    if gt_boxes is None:
+        return image_paded
+    else:
+        gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
+        gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
+        return image_paded, gt_boxes
